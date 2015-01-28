297b17782d1e07749e86e1a88d1b97a7b0ca3dfe
Ryan Johnson <escattone@gmail.com>
2015-01-27 20:48:07 -0800

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(relpath):
 
 setup(
     name='txpool',
-    version='0.9',
+    version='0.9.1',
     description='A persistent process pool in Python for Twisted',
     long_description=read('README.rst'),
     license='MIT',
